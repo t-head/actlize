6821eaaedce11f4e08e6841859ccd4822f024afd
ziheng.czh <ziheng.czh@alibaba-inc.com>
2026-09-23 18:51:30 +0800

    actlize v0.5.0: __brkpt is only declared for device side

--- include/cutlass/arch/arch.h
+++ include/cutlass/arch/arch.h
@@ -46,7 +46,10 @@ struct PPU0015 {
 /// Triggers a breakpoint on the device
 CUTLASS_DEVICE
 void device_breakpoint() {
+#if defined(__HGGC_ARCH__)
+  // __brkpt is only declared for device side.
   __brkpt();
+#endif
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
